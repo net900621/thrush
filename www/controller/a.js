b02089958e9acd7baafebed9d675fbbc2af6fc84
yaoyao <net900621@163.com>
2015-01-11 21:08:39 +0800

    test

--- www/controller/a.js
+++ www/controller/a.js
@@ -6,7 +6,6 @@ var conf = {
 		var data = {'value' : '/welcome'},
 			self = this;
 		this.setData(data);
-		console.log(this.listenStack);
 		this.listen(function(data){
 			return self.render('welcome', data);
 		});
