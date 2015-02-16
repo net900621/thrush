33ea7b47450f081ef950d711e9bcace0b26c89b7
net900621 <net900621@163.com>
2015-02-16 16:54:04 +0800

    er mode

--- statics/js/page/pc.js
+++ statics/js/page/pc.js
@@ -1,3 +1,13 @@
-fml.define('page/pc', ['jquery'], function(require, exports) {
-	
+fml.define('page/pc', ['jquery' , 'common/QRCode'], function(require, exports) {
+	var QRCode = require('common/QRCode'),
+		list = $('.listInfo');
+	list.forEach(function(v, i){debugger;
+		$('#qrcode_c').html('')
+		var qr = v.find('.er');
+		var qrcode = new QRCode(qr, {  
+	        width : qr.width(),  
+	        height : qr.height()  
+	    });
+	    qrcode.makeCode(v.find('a').attr('href'));
+	});
 });
