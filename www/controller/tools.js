2eefc37d411bb9b11adb2dd050013a5a3ec0cc3b
net900621 <net900621@163.com>
2015-12-29 13:10:45 +0800

    yaoyao

---
+++ www/controller/tools.js
@@ -0,0 +1,16 @@
+var tools = function() {
+	return this;
+};
+var conf = {
+	'index' : function(){
+		var data = {
+		},
+			self = this;
+		this.setData(data);
+		this.listen(function(data){
+			self.cssLink = ['tools'];
+			return self.render('tools', data);
+		});
+	}
+};
+exports.__create = controller.__create(tools, conf);
